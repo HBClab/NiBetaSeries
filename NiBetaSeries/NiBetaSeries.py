fbedf806adfd359ba176d41bbc4b28b50ce5894e
jdkent <james-kent@uiowa.edu>
2017-09-01 13:48:17 -0500

    WIP: BIDS code sprint

--- NiBetaSeries/NiBetaSeries.py
+++ NiBetaSeries/NiBetaSeries.py
@@ -1,211 +1,127 @@
-from __future__ import absolute_import, division, print_function
-import numpy as np
-import pandas as pd
-import scipy.optimize as opt
-from scipy.special import erf
-from .due import due, Doi
+import os
+from nilearn import plotting
+from nistats.first_level_model import first_level_models_from_bids
+import nibabel as nib
+import argparse
+import subprocess
+import numpy
+from glob import glob
+#BIDS
+# handle registration
+#    transform bold to atlas
+#    use same transform for brainmask
+__version__ = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                'version')).read()
 
-__all__ = ["Model", "Fit", "opt_err_func", "transform_data", "cumgauss"]
+def run(command, env={}):
+    merged_env = os.environ
+    merged_env.update(env)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT, shell=True,
+                               env=merged_env)
+    while True:
+        line = process.stdout.readline()
+        line = str(line, 'utf-8')[:-1]
+        print(line)
+        if line == '' and process.poll() != None:
+            break
+    if process.returncode != 0:
+        raise Exception("Non zero return code: %d"%process.returncode)
 
+parser = argparse.ArgumentParser(description='BetaSeries BIDS arguments')
+parser.add_argument('bids_dir', help='The directory with the input dataset '
+                    'formatted according to the BIDS standard.')
+parser.add_argument('derivatives_pipeline', help='The pipeline that contains minimally preprocessed '
+                    'img, brainmask, and confounds.tsv')
+parser.add_argument('output_dir', help='The directory where the output files '
+                    'should be stored. If you are running group level analysis '
+                    'this folder should be prepopulated with the results of the'
+                    'participant level analysis.')
+parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
+                    'Multiple participant level analyses can be run independently '
+                    '(in parallel) using the same output_dir.',
+                    choices=['participant', 'group'])
+parser.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
+                   'corresponds to sub-<participant_label> from the BIDS spec '
+                   '(so it does not include "sub-"). If this parameter is not '
+                   'provided all subjects should be analyzed. Multiple '
+                   'participants can be specified with a space separated list.',
+                   nargs="+")
+parser.add_argument('-v', '--version', action='version',
+                    version='nistat_betaseries {}'.format(__version__))
+# Derivative Specific Options
+derivatives =  parser.add_argument_group('Options for derivatives')
+derivatives.add_argument('-t', '--task_id', action='store',
+                         help='select a specific task to be processed')
+derivatives.add_argument('-s', '--space', action='store',
+                         help='select a bold derivative in a specific space to be used')
+derivatives.add_argument('--variant', action='store',
+                         help='select a variant bold to process')
+derivatives.add_argument('-r', '--res', action='store',
+                         help='select a resolution to analyze')
+# BetaSeries Specific Options
+beta_series = parser.add_argument_group('Options for processing beta_series')
+#
+beta_series.add_argument('--hrf_model', help='',
+                         choices=['glover', 'spm', 'fir',
+                         'gloverDerivative',
+                         'gloverDerivativeDispersion',
+                         'spmDerivative'
+                         'spmDerivativeDispersion'])
 
-# Use duecredit (duecredit.org) to provide a citation to relevant work to
-# be cited. This does nothing, unless the user has duecredit installed,
-# And calls this with duecredit (as in `python -m duecredit script.py`):
-due.cite(Doi("10.1167/13.9.30"),
-         description="Template project for small scientific Python projects",
-         tags=["reference-implementation"],
-         path='BetaSeries')
+beta_series.add_argument()
+args = parser.parse_args()
 
+run('bids-validator %s'%args.bids_dir)
 
-def transform_data(data):
-    """
-    Function that takes experimental data and gives us the
-    dependent/independent variables for analysis.
+subjects_to_analyze = []
+# only for a subset of subjects
+if args.participant_label:
+    subjects_to_analyze = args.participant_label
+# for all subjects
+else:
+    subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
+    subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
 
-    Parameters
-    ----------
-    data : Pandas DataFrame or string.
-        If this is a DataFrame, it should have the columns `contrast1` and
-        `answer` from which the dependent and independent variables will be
-        extracted. If this is a string, it should be the full path to a csv
-        file that contains data that can be read into a DataFrame with this
-        specification.
-
-    Returns
-    -------
-    x : array
-        The unique contrast differences.
-    y : array
-        The proportion of '2' answers in each contrast difference
-    n : array
-        The number of trials in each x,y condition
-    """
-    if isinstance(data, str):
-        data = pd.read_csv(data)
-
-    contrast1 = data['contrast1']
-    answers = data['answer']
-
-    x = np.unique(contrast1)
-    y = []
-    n = []
-
-    for c in x:
-        idx = np.where(contrast1 == c)
-        n.append(float(len(idx[0])))
-        answer1 = len(np.where(answers[idx[0]] == 1)[0])
-        y.append(answer1 / n[-1])
-    return x, y, n
-
-
-def cumgauss(x, mu, sigma):
-    """
-    The cumulative Gaussian at x, for the distribution with mean mu and
-    standard deviation sigma.
-
-    Parameters
-    ----------
-    x : float or array
-       The values of x over which to evaluate the cumulative Gaussian function
-
-    mu : float
-       The mean parameter. Determines the x value at which the y value is 0.5
-
-    sigma : float
-       The variance parameter. Determines the slope of the curve at the point
-       of Deflection
-
-    Returns
-    -------
-
-    g : float or array
-        The cumulative gaussian with mean $\\mu$ and variance $\\sigma$
-        evaluated at all points in `x`.
-
-    Notes
-    -----
-    Based on:
-    http://en.wikipedia.org/wiki/Normal_distribution#Cumulative_distribution_function
-
-    The cumulative Gaussian function is defined as:
-
-    .. math::
-
-        \\Phi(x) = \\frac{1}{2} [1 + erf(\\frac{x}{\\sqrt{2}})]
-
-    Where, $erf$, the error function is defined as:
-
-    .. math::
-
-        erf(x) = \\frac{1}{\\sqrt{\\pi}} \int_{-x}^{x} e^{t^2} dt
-
-    """
-    return 0.5 * (1 + erf((x - mu) / (np.sqrt(2) * sigma)))
+# running participant level
+if args.analysis_level == "participant":
 
 
-def opt_err_func(params, x, y, func):
-    """
-    Error function for fitting a function using non-linear optimization.
+# check docs about hrf_model
 
-    Parameters
-    ----------
-    params : tuple
-        A tuple with the parameters of `func` according to their order of
-        input
+models, models_run_imgs, models_events, models_confounds = first_level_models_from_bids(
+        data_dir2, task_label, space_label,
+        t_r=2.0, slice_time_ref=0.5,
+        hrf_model='glover + derivative + dispersion',
+        #find a general mask?
+        mask='template',
+        signal_scaling=0, verbose=3, n_jobs=-2,
+        derivatives_folder=derivatives_folder,
+        img_filters=[('variant', 'smoothAROMAnonaggr')])
 
-    x : float array
-        An independent variable.
+for sub_idx,sub_model in enumerate(models):
+    for run_idx, run_events in enumerate(models_events[sub_idx]):
+        run_events.sort_values(columns=['trial_type', 'onset'],ascending=[True, True],inplace=True,axis=0)
+        run_events_temp = run_events.copy()
+        run_events_temp['trial_type'] = 'other_trials'
+        for trial in range(len(run_events)):
+            #if the condition changes (make sure to order the conditions)
+            if trial != 0 and run_events.loc[trial, 'trial_type'] != run_events.loc[trial-1, 'trial_type']:
 
-    y : float array
-        The dependent variable.
-
-    func : function
-        A function with inputs: `(x, *params)`
-
-    Returns
-    -------
-    float array
-        The marginals of the fit to x/y given the params
-    """
-    return y - func(x, *params)
-
-
-class Model(object):
-    """Class for fitting cumulative Gaussian functions to data"""
-    def __init__(self, func=cumgauss):
-        """ Initialize a model object.
-
-        Parameters
-        ----------
-        data : Pandas DataFrame
-            Data from a subjective contrast judgement experiment
-
-        func : callable, optional
-            A function that relates x and y through a set of parameters.
-            Default: :func:`cumgauss`
-        """
-        self.func = func
-
-    def fit(self, x, y, initial=[0.5, 1]):
-        """
-        Fit a Model to data.
-
-        Parameters
-        ----------
-        x : float or array
-           The independent variable: contrast values presented in the
-           experiment
-        y : float or array
-           The dependent variable
-
-        Returns
-        -------
-        fit : :class:`Fit` instance
-            A :class:`Fit` object that contains the parameters of the model.
-
-        """
-        params, _ = opt.leastsq(opt_err_func, initial,
-                                args=(x, y, self.func))
-        return Fit(self, params)
-
-
-class Fit(object):
-    """
-    Class for representing a fit of a model to data
-    """
-    def __init__(self, model, params):
-        """
-        Initialize a :class:`Fit` object.
-
-        Parameters
-        ----------
-        model : a :class:`Model` instance
-            An object representing the model used
-
-        params : array or list
-            The parameters of the model evaluated for the data
-
-        """
-        self.model = model
-        self.params = params
-
-    def predict(self, x):
-        """
-        Predict values of the dependent variable based on values of the
-        indpendent variable.
-
-        Parameters
-        ----------
-        x : float or array
-            Values of the independent variable. Can be values presented in
-            the experiment. For out-of-sample prediction (e.g. in
-            cross-validation), these can be values
-            that were not presented in the experiment.
-
-        Returns
-        -------
-        y : float or array
-            Predicted values of the dependent variable, corresponding to
-            values of the independent variable.
-        """
-        return self.model.func(x, *self.params)
+            run_events_temp.loc[trial, 'trial_type'] = run_events.loc[trial, 'trial_type']
+            #fitmodel
+            sub_model.fit(models_run_imgs[sub_idx][run_idx],run_events_temp)
+            #compute contrast
+            img = sub_model.compute_contrast(contrast_def=run_events_temp.loc[trial, 'trial_type'], output_type='effect_size')
+            #save img
+            nib.save(img, os.path.join(data_dir2,'derivatives/nistats_betaseries/sub-GE140_2','sub-GE140'+run_events_temp.loc[trial,'trial_type']+str(trial)))
+            run_events_temp.loc[trial, 'trial_type'] = 'other_trials'
+            
+# running group level
+elif args.analysis_level == "group":
+    brain_sizes = []
+    for subject_label in subjects_to_analyze:
+        for brain_file in glob(os.path.join(args.output_dir, "sub-%s*.nii*"%subject_label)):
+            data = nib.load(brain_file).get_data()
+            # calcualte average mask size in voxels
+            brain_sizes.append((data != 0).sum())
