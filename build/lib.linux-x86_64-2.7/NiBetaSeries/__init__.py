4a74995676eb501c82507833fd3bafcd8603c35c
jdkent <james-kent@uiowa.edu>
2017-09-08 23:48:44 -0500

    WIP: borrowing fmriprep code

---
+++ build/lib.linux-x86_64-2.7/NiBetaSeries/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+from __future__ import absolute_import, division, print_function
+from .version import (
+    __version__,
+    AUTHOR,
+    LICENSE,
+    MAINTAINER,
+    MAINTAINER_EMAIL,
+    URL,
+    NAME,
+    DESCRIPTION,
+    LONG_DESCRIPTION
+)
