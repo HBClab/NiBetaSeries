2f583852a9544a8d00c8dfb21169ff225c234062
jdkent <james-kent@uiowa.edu>
2017-10-13 17:31:55 -0500

    WIP: logic nipype workflow

--- NiBetaSeries/workflows/analysis.py
+++ NiBetaSeries/workflows/analysis.py
@@ -18,24 +18,45 @@ from niworkflows.nipype.interfaces.ants import ApplyTransforms
 from niworkflows.nipype.interfaces.afni.preprocess import TCorr1D
 from niworkflows.nipype.interfaces.afni.utils import Calc
 
+# okay to get this to work correctly I need to think:
+# step 1: parse_mni_roi_coords_tsv
+#   inputs: nothing to iterate over
+#   outputs: 2 lists of strings [op_strings], [out_name_strings]
+# step 2: make_roi (need to iterate over op_strings and out_name_strings)
+#   inputs: op_string, out_name_string (iterate over both)
+#   outputs: roi_mni.nii.gz
+#   Use MapNode to get a list of outputs
+#   revised outputs: [roi1_mni.nii.gz, roi2_mni.nii.gz ... roix_mni.nii.gz]
+# step 3: roi_mni2t1w_transform
+#   inputs: iterate over input image
+#   outputs: [roi1_bold.nii.gz, roi2_bold.nii.gz ... roix_bold.nii.gz]
+#   Use MapNode to iterate over the rois
+# step 4: extract_signal
+#   inputs: iterate over rois (nested iterate over betaseries_files)
+#   outputs: [roi1.txt, roi2.txt ... roix.txt]
+#   iterables = betaseries_files (so a separate list of rois.txt corresponding
+#       to each betaseries_file)
 
 def init_correlation_wf(roi_radius=12, name="correlation_wf"):
+    # transform subject coordinates (mm) to voxel coordinates
     def xyz2ijk(xyz, affine):
         import numpy as np
         M_prime = np.linalg.inv(affine[:3, :3])
         abc = affine[:3, 3]
         return M_prime.dot(xyz) - abc
 
+    # transform voxel coordinates to subject coordinates (mm)
     def ijk2xyz(ijk, affine):
         M = affine[:3, :3]
         abc = affine[:3, 3]
         return M.dot(ijk) + abc
 
+    # provides the input to make the roi using ImageMaths
     def parse_mni_roi_coords_tsv(mni_roi_coords, radius, mni_img):
         import csv
         import nibabel as nib
-        # return op_string
-        # return name of roi
+
+        # base string for ImageMaths command
         op_string = """\
                   -mul 0 -add 1 -roi {x} 1 {y} 1 {z} 1 0 1 \
                   -kernel sphere {radius} -fmean -bin \
@@ -50,6 +71,8 @@ def init_correlation_wf(roi_radius=12, name="correlation_wf"):
             reader = csv.DictReader(tsvfile, delimiter='\t')
             for row in reader:
                 out_names.append(row.pop('name')+'_{}mm.nii.gz'.format(radius))
+                # transfer the subject coordinates in the tsv to
+                # voxel coordinates for the image
                 ijk_row = {
                            coord: int(round(point))
                            for coord, point in
@@ -57,14 +80,16 @@ def init_correlation_wf(roi_radius=12, name="correlation_wf"):
                                xyz2ijk([row['x'], row['y'], row['z']],
                                        nib_img.affine))
                 }
+                # add radius to args dictionary
                 ijk_row['radius'] = roi_radius
                 op_inputs.append(op_string.format(**ijk_row))
 
+        # each is a list of outputs
         return op_inputs, out_names
 
     workflow = pe.Workflow(name=name)
 
-    inputnode = pe.Node(niu.IdentityInterface(fields=['betas',
+    inputnode = pe.Node(niu.IdentityInterface(fields=['betaseries_files',
                                                       'bold_mask',
                                                       'mni_roi_coords',
                                                       't1w_space_mni_mask',
@@ -72,29 +97,44 @@ def init_correlation_wf(roi_radius=12, name="correlation_wf"):
                                                       'target_mni_warp']),
                         name='inputnode')
 
+    betaseries_file = pe.Node(niu.IdentityInterface(fields=['betaseries_file']),
+                              name='betaseries_file',
+                              iterables=('betaseries_file', inputnode.inputs.betaseries_files))
+
     outputnode = pe.Node(niu.IdentityInterface(fields=['zmaps_mni']),
                          name='outputnode')
 
-    # make the rois first
-    # fslmaths -roi uses ijk coordinates
-    make_roi = pe.MapNode(ImageMaths())
-
+    # prep the input for make_roi
     parse_roi_tsv = pe.Node(niu.Function(input_names=['mni_roi_coords', 'radius', 'mni_img'],
                                          output_names=['op_inputs', 'out_names'],
                                          function=parse_mni_roi_coords_tsv),
                             name='parse_roi_tsv')
+
+    # make the rois first
+    # fslmaths -roi uses ijk coordinates
+    # iterate over roi coordinate + name of roi
+    make_roi = pe.MapNode(ImageMaths(),
+                          name='make_roi',
+                          iterfield=['in_file', 'out_file'])
+
+
 
     # transform rois to subject space
     # iterfield is infile
     roi_mni2t1w_transform = pe.MapNode(ApplyTransforms(interpolation='NearestNeighbor'),
                                        name='roi_mni2t1w_transform')
 
+
     # get the mean signal from a subject roi
-    extract_signal = pe.MapNode(ImageMeants())
+    extract_signal = pe.MapNode(ImageMeants(),
+                                name='extract_signal',
+                                iterfield=['mask'])
+    extract_signal.iterables = ('in_file', inputnode.inputs.betaseries_files)
 
     # correlation
     pearsons_corr = pe.Node(TCorr1D(outputtype='NIFTI_GZ', pearson=True),
                             name='3dTCorr1D')
+    pearsons_corr.iterables = ('xset', inputnode.inputs.betaseries_files)
 
     # pearson's r to z transfer
     # 3dcalc -a Corr_subj01+tlrc -expr 'log((1+a)/(1-a))/2' -prefix Corr_subj01_Z+tlrc
@@ -107,18 +147,21 @@ def init_correlation_wf(roi_radius=12, name="correlation_wf"):
                                         name='zmap_t1w2mni_transform')
 
     workflow.connect([
+        # set up the rois (same rois will be used for each 4d betaseries file)
         (inputnode, parse_roi_tsv, [('mni_roi_coords', 'mni_roi_coords'),
                                     ('roi_radius', 'radius'),
                                     ('t1w_space_mni_mask', 'mni_img')]),
+        # parse_roi_tsv returns a list
         (parse_roi_tsv, make_roi, [('op_inputs', 'op_string'),
                                    ('out_names', 'out_file')]),
         (inputnode, make_roi, [('t1w_space_mni_mask', 'in_file')]),
+        # iterate over rois
         (make_roi, roi_mni2t1w_transform, [('out_file', 'input_image')]),
         (inputnode, roi_mni2t1w_transform, [('bold_mask', 'reference_image'),
                                             ('target_t1w_warp', 'transforms')]),
         (roi_mni2t1w_transform, extract_signal, [('output_image', 'mask')]),
-        (inputnode, extract_signal, [('betas', 'in_file')]),
-        (inputnode, pearsons_corr, [('betas', 'xset')]),
+        # (inputnode, extract_signal, [('betaseries_files', 'in_file')]),
+        # (inputnode, pearsons_corr, [('betaseries_files', 'xset')]),
         (extract_signal, pearsons_corr, [('out_file', 'y_1d')]),
         (pearsons_corr, p_to_z, [('out_file', 'in_file_a')]),
         (p_to_z, zmap_t1w2mni_transform, [('out_file', 'input_image')]),
