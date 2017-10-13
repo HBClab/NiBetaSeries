8a2674c82be7220d34d64fc0a2a61eea4d1091f4
jdkent <james-kent@uiowa.edu>
2017-10-13 13:05:32 -0500

    ENH: analysis.py flake8 conform

--- NiBetaSeries/workflows/analysis.py
+++ NiBetaSeries/workflows/analysis.py
@@ -18,7 +18,8 @@ from niworkflows.nipype.interfaces.ants import ApplyTransforms
 from niworkflows.nipype.interfaces.afni.preprocess import TCorr1D
 from niworkflows.nipype.interfaces.afni.utils import Calc
 
-def init_single_subject_correlation_wf():
+
+def init_correlation_wf(roi_radius=12, name="correlation_wf"):
     def xyz2ijk(xyz, affine):
         import numpy as np
         M_prime = np.linalg.inv(affine[:3, :3])
@@ -29,7 +30,6 @@ def init_single_subject_correlation_wf():
         M = affine[:3, :3]
         abc = affine[:3, 3]
         return M.dot(ijk) + abc
-
 
     def parse_mni_roi_coords_tsv(mni_roi_coords, radius, mni_img):
         import csv
@@ -49,11 +49,16 @@ def init_single_subject_correlation_wf():
         with open(mni_roi_coords) as tsvfile:
             reader = csv.DictReader(tsvfile, delimiter='\t')
             for row in reader:
-                out_names.append(row.pop('name')+'_{}.nii.gz'.format(radius))
-                ijk_row = {coord : int(round(point))
-                    for coord,point in
-                    zip(['x', 'y', 'z'], xyz2ijk([row['x'], row['y'], row['z']], affine))}
-                op_inputs.append(op_string.format(**ijk_row, radius=radius))
+                out_names.append(row.pop('name')+'_{}mm.nii.gz'.format(radius))
+                ijk_row = {
+                           coord: int(round(point))
+                           for coord, point in
+                           zip(['x', 'y', 'z'],
+                               xyz2ijk([row['x'], row['y'], row['z']],
+                                       nib_img.affine))
+                }
+                ijk_row['radius'] = roi_radius
+                op_inputs.append(op_string.format(**ijk_row))
 
         return op_inputs, out_names
 
@@ -62,31 +67,16 @@ def init_single_subject_correlation_wf():
     inputnode = pe.Node(niu.IdentityInterface(fields=['betas',
                                                       'bold_mask',
                                                       'mni_roi_coords',
-                                                      'roi_radius',
                                                       't1w_space_mni_mask',
                                                       'target_t1w_warp',
                                                       'target_mni_warp']),
                         name='inputnode')
 
     outputnode = pe.Node(niu.IdentityInterface(fields=['zmaps_mni']),
-                          name='outputnode')
-
-    # transform_mni_coords_2_t1 = pe.Node(ApplyTransformsToPoints(),
-    #                            name='transform_mni_coords_2_t1')
-    # input:
-    #   input_file: mni_roi_coords
-    #   transforms: warp
-    #   dimension = 3
-    # output:
-    #   output_file: csv with transformed coordinates
-    # TODO: check if the coordinates are resliced
+                         name='outputnode')
 
     # make the rois first
-    # use fslmaths -roi specify MNI coordinates, (xyz, not ijk)
-    # fslmaths avg152T1.nii.gz -mul 0 -add 1 -roi 45 1 74 1 51 1 0 1 ACCpoint -odt float
-    # fslmaths ACCpoint -kernel sphere 5 -fmean ACCsphere -odt float
-    # fslmaths sub-controlGE154_T1w_space-MNI152NLin2009cAsym_brainmask.nii.gz -mul 0 -add 1 -roi 45 1 74 1 51 1 0 1 -kernel sphere 5 -fmean -bin sphere_test2.nii.gz
-
+    # fslmaths -roi uses ijk coordinates
     make_roi = pe.MapNode(ImageMaths())
 
     parse_roi_tsv = pe.Node(niu.Function(input_names=['mni_roi_coords', 'radius', 'mni_img'],
@@ -104,14 +94,14 @@ def init_single_subject_correlation_wf():
 
     # correlation
     pearsons_corr = pe.Node(TCorr1D(outputtype='NIFTI_GZ', pearson=True),
-                      name='3dTCorr1D')
+                            name='3dTCorr1D')
 
     # pearson's r to z transfer
     # 3dcalc -a Corr_subj01+tlrc -expr 'log((1+a)/(1-a))/2' -prefix Corr_subj01_Z+tlrc
     p_to_z = pe.Node(Calc(expr='log((1+a)/(1-a))/2'))
 
-
-    # TODO: rename warps ds005/out/fmriprep/sub-01/anat/sub-01_T1w_target-MNI152NLin2009cAsym_warp.h5
+    # TODO: rename warps
+    # ds005/out/fmriprep/sub-01/anat/sub-01_T1w_target-MNI152NLin2009cAsym_warp.h5
     # transform to standard space
     zmap_t1w2mni_transform = pe.MapNode(ApplyTransforms(interpolation="LanczosWindowedSinc"),
                                         name='zmap_t1w2mni_transform')
@@ -121,7 +111,7 @@ def init_single_subject_correlation_wf():
                                     ('roi_radius', 'radius'),
                                     ('t1w_space_mni_mask', 'mni_img')]),
         (parse_roi_tsv, make_roi, [('op_inputs', 'op_string'),
-                                   ('out_names', 'out_file'])),
+                                   ('out_names', 'out_file')]),
         (inputnode, make_roi, [('t1w_space_mni_mask', 'in_file')]),
         (make_roi, roi_mni2t1w_transform, [('out_file', 'input_image')]),
         (inputnode, roi_mni2t1w_transform, [('bold_mask', 'reference_image'),
@@ -135,7 +125,6 @@ def init_single_subject_correlation_wf():
         (inputnode, zmap_t1w2mni_transform, [('t1w_space_mni_mask', 'reference_image'),
                                              ('target_mni_warp', 'transforms')]),
         (zmap_t1w2mni_transform, outputnode, [('output_image', 'zmaps_mni')]),
-
     ])
 
     return workflow
