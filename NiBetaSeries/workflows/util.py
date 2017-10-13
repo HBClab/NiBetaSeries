2fdbf6ca475878023c740649850c64985a5c087a
jdkent <james-kent@uiowa.edu>
2017-10-13 12:29:56 -0500

    ENH: util.py flake8 conform/cleanup

--- NiBetaSeries/workflows/util.py
+++ NiBetaSeries/workflows/util.py
@@ -7,160 +7,8 @@ Workflow for getting subject info
 -presumes fmriprep has run, expects directories to both BIDS data and fmriprep output
 '''
 from __future__ import print_function, division, absolute_import, unicode_literals
+import os
 
-import pkg_resources as pkgr
-from bids.grabbids import BIDSLayout
-from nipype.interfaces.base import Bunch
-from nipype.interfaces.utility import IdentityInterface
-import nipype.interfaces.io as nio
-from niworkflows.nipype import logging
-import pandas as pd
-import numpy as np
-
-def get_bids_data(bids_dir):
-    config_file=pkgr.resource_filename('NiBetaSeries', 'utils/bids_derivatives.json')
-    return BIDSLayout(bids_dir,config=config_file)
-
-# TODO: incorporate run into analysis
-def get_bold_info(bids_data, task, subject, run=None):
-    import json
-    from collections import Counter
-    logger = logging.getLogger('interface')
-
-    # get the TR
-    global_json_list = bids_data.get(extensions='task-'+task+'_bold.json',
-                                     type='bold',
-                                     task=task,
-                                     return_type='file')
-
-    if len(global_json_list) > 1:
-        logger.error('ERROR: More than one global json for the task')
-    elif len(global_json_list) == 0:
-        logger.warn('WARNING: No global json for the task')
-        global_json = None
-    else:
-        with open(global_json_list[0]) as glob_info:
-            global_json = Counter(json.load(glob_info))
-
-    subject_json_list = bids_data.get(extensions='json',
-                                      type='bold',
-                                      task=task,
-                                      subject=subject,
-                                      return_type='file')
-
-    if len(subject_json_list) > 1:
-        logger.error('ERROR: More than one subject json for the task')
-    elif len(global_json_list) == 0:
-        logger.warn('WARNING: No subject json for the task')
-        subject_json = None
-    else:
-        with open(subject_json_list[0]) as sub_info:
-            subject_json = Counter(json.load(sub_info))
-
-    if subject_json is not None and global_json is not None:
-        bold_parameters = subject_json + global_json
-    elif subject_json is not None:
-        bold_parameters = subject_json
-    elif global_json is not None:
-        bold_parameters = global_json
-
-    repetition_time = bold_parameters['RepetitionTime']
-
-    return repetition_time
-
-
-def get_confound_info(derivatives_data, task, subject, run=None, regressor_names=None):
-    logger = logging.getLogger('interface')
-
-    if run is not None:
-        task_confounds_list = bids_data.get(extensions='task-'+task+'_confounds.tsv',
-                                            type='confounds',
-                                            task=task,
-                                            run=run,
-                                            return_type='file')
-    else:
-        task_confounds_list = bids_data.get(extensions='task-'+task+'_confounds.tsv',
-                                            type='confounds',
-                                            task=task,
-                                            return_type='file')
-
-    # There should exactly one confound file
-    if len(task_confounds_list) > 1:
-        logger.error('ERROR: More than one confound tsv for the task')
-    elif len(task_confounds_list) == 0:
-        logger.error('ERROR: No confound tsv for the task')
-    else:
-        confounds_pd = pd.read_csv(task_confounds_list[0], sep="\t")
-
-    if regressor_names == None:
-        confound_names = [col for col in confound_pd.columns
-                             if 'CompCor' in col or 'X' in col or 'Y' in col or 'Z' in col]
-        confound_pd_filt = confound_pd.filter(items=confound_names)
-
-        confounds = confound_pd_filt.values.swapaxes(0,1).tolist()
-
-    return confound_names, confounds
-
-
-def get_task_info(bids_data, task, subject, run=None):
-    logger = logging.getLogger('interface')
-
-    if run is not None:
-        task_event_list = bids_data.get(extensions='tsv',
-                                            type='events',
-                                            task=task,
-                                            run=run,
-                                            return_type='file')
-    else:
-        task_event_list = bids_data.get(extensions='tsv',
-                                            type='events',
-                                            task=task,
-                                            return_type='file')
-
-    if len(task_event_list) > 1:
-        logger.error('ERROR: More than one event tsv for the task')
-    elif len(task_event_list) == 0:
-        logger.error('ERROR: No event tsv for the task')
-    else:
-        events_pd = pd.read_csv(task_event_list[0], sep="\t")
-
-    events_pd_grouped = events_pd[['onset', 'duration', 'trial_type']].groupby('trial_type')
-
-    conditions = list(events_pd_grouped.groups.keys())
-    onsets = [ events_pd_grouped.get_group(condition)[['onset']].values.flatten().tolist() ]
-    durations = [ events_pd_grouped.get_group(condition)[['duration']].values.flatten().tolist() ]
-
-    return conditions, onsets, durations
-
-def bunch_info(conditions, onsets, durations, regressor_names, regressors):
-    return Bunch(conditions=conditions,
-                onsets=onsets,
-                durations=durations,
-                regressors=regressors,
-                regressor_names=regressor_names)
-
-def init_subject_info_wf(name='subject_info_wf'):
-    input_node = pe.Node(IdentityInterface(fields=['subject', 'bids_dir', 'hrf_model', 'fmriprep_dir']),
-                         name='input_node')
-
-    output_node = pe.Node(IdentityInterface(fields=['subject_info', 'input_units',
-                                                     'time_repetition']),
-                           name='output_node')
-
-    bids_data_node = pe.Node(
-        utility.Function(function=get_bids_data,
-                         input_names=['bids_dir'],
-                         output_names=['bids_data']),
-        name='bids_data_node')
-
-    confound_info_node = pe.Node(
-        utility.Function(function=get_confound_info,
-                         input_names=['derivatives_data', 'task', 'subject', 'run'],
-                         output_names=['confound_names', 'confounds']),
-        name='confound_info_node')
-
-    # TODO: get tasks
-    confound_info_node.iterables = ("task", tasks)
 
 class BIDSError(ValueError):
     def __init__(self, message, bids_root):
@@ -202,10 +50,10 @@ def collect_participants(bids_dir, participant_label=None, strict=False):
     fmriprep.utils.bids.BIDSError:
     ...
     """
-    bids_dir = op.abspath(bids_dir)
+    bids_dir = os.path.abspath(bids_dir)
     all_participants = sorted(
         [subdir[4:] for subdir in os.listdir(bids_dir)
-         if op.isdir(op.join(bids_dir, subdir)) and subdir.startswith('sub-')])
+         if os.path.isdir(os.path.join(bids_dir, subdir)) and subdir.startswith('sub-')])
 
     # Error: bids_dir does not contain subjects
     if not all_participants:
@@ -240,12 +88,13 @@ def collect_participants(bids_dir, participant_label=None, strict=False):
             ', '.join(notfound_label)), bids_dir)
         if strict:
             raise exc
-        warnings.warn(exc.msg, BIDSWarning)
+        # warnings.warn(exc.msg, BIDSWarning)
 
     return found_label
 
 
-def collect_data(layout, participant_label, deriv=False, ses=None, task=None, run=None, space=None):
+def collect_data(layout, participant_label, deriv=False, ses=None,
+                 task=None, run=None, space=None):
     """
     Uses grabbids to retrieve the input data for a given participant
     >>> bids_root, _ = collect_data('ds054', '100185')
@@ -274,22 +123,57 @@ def collect_data(layout, participant_label, deriv=False, ses=None, task=None, ru
     """
     if deriv:
         queries = {
-            'preproc': {'subject': participant_label, 'modality': 'func', 'type': 'preproc',
-                     'extensions': ['nii', 'nii.gz']},
-            'bold_brainmask': {'subject': participant_label, 'modality': 'func', 'type': 'brainmask',
-                      'extensions': ['nii', 'nii.gz']},
-            'mni_brainmask': {'subject': participant_label, 'modality': 'anat', 'type': 'brainmask',
-                       'space': 'MNI152NLin2009cAsym', 'extensions': ['nii', 'nii.gz']},
-            'AROMAnoiseICs': {'subject': participant_label, 'modality': 'func', 'type': 'AROMAnoiseICs',
-                    'extensions': '.csv'},
-            'MELODICmix': {'subject': participant_label, 'modality': 'func', 'type': 'MELODICmix',
-                    'extensions': 'tsv'},
-            'confounds': {'subject': participant_label, 'modality': 'func', 'type': 'confounds',
-                    'extensions': 'tsv'},
-            'target_t1w_warp': {'subject': participant_label, 'modality': 'anat', 'type': 'warp',
-                'target': 'T1w', 'extensions': 'h5'},
-            'target_mni_warp': {'subject': participant_label, 'modality': 'anat', 'type': 'warp',
-                'target': 'MNI152NLin2009cAsym', 'extensions': 'h5'},
+            'preproc': {
+                'subject': participant_label,
+                'modality': 'func',
+                'type': 'preproc',
+                'extensions': ['nii', 'nii.gz']
+            },
+            'bold_brainmask': {
+                'subject': participant_label,
+                'modality': 'func',
+                'type': 'brainmask',
+                'extensions': ['nii', 'nii.gz']
+            },
+            'mni_brainmask': {
+                'subject': participant_label,
+                'modality': 'anat',
+                'type': 'brainmask',
+                'space': 'MNI152NLin2009cAsym',
+                'extensions': ['nii', 'nii.gz']
+            },
+            'AROMAnoiseICs': {
+                'subject': participant_label,
+                'modality': 'func',
+                'type': 'AROMAnoiseICs',
+                'extensions': '.csv'
+            },
+            'MELODICmix': {
+                'subject': participant_label,
+                'modality': 'func',
+                'type': 'MELODICmix',
+                'extensions': 'tsv'
+            },
+            'confounds': {
+                'subject': participant_label,
+                'modality': 'func',
+                'type': 'confounds',
+                'extensions': 'tsv'
+            },
+            'target_t1w_warp': {
+                'subject': participant_label,
+                'modality': 'anat',
+                'type': 'warp',
+                'target': 'T1w',
+                'extensions': 'h5'
+            },
+            'target_mni_warp': {
+                'subject': participant_label,
+                'modality': 'anat',
+                'type': 'warp',
+                'target': 'MNI152NLin2009cAsym',
+                'extensions': 'h5'
+            },
         }
 
         if task:
@@ -316,8 +200,12 @@ def collect_data(layout, participant_label, deriv=False, ses=None, task=None, ru
 
     else:
         queries = {
-            'events': {'subject': participant_label, 'modality': 'func', 'type': 'events',
-                    'extensions': 'tsv'},
+            'events': {
+                'subject': participant_label,
+                'modality': 'func',
+                'type': 'events',
+                'extensions': 'tsv'
+            },
         }
 
         if run:
