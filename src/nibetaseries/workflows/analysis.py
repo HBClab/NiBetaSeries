6aadd4464d87b1dfcc31c7ff7c2d6a7538d22ea7
James Kent <james-kent@uiowa.edu>
2018-07-15 22:34:56 -0500

    work with mapnode

--- src/nibetaseries/workflows/analysis.py
+++ src/nibetaseries/workflows/analysis.py
@@ -42,21 +42,41 @@ def init_correlation_wf(name="correlation_wf"):
     """
     workflow = pe.Workflow(name=name)
 
-    input_node = pe.Node(niu.IdentityInterface(fields=['betaseries_file',
-                                                       'atlas_file',
-                                                       'atlas_lut']),
-                         name='input_node')
+    def _rename_matrix(correlation_matrix, betaseries_file):
+        import os
+        import re
+        from shutil import copyfile
+
+        betaseries_regex = re.compile('betaseries_trialtype-(P?<trial_type>[A-Za-z0-9]+).nii.gz')
+        trial_type = betaseries_regex.search(betaseries_file).groupdict()['trial_type']
+        out_file = os.path.join(os.getcwd(),
+                                'correlation-matrix_trialtype-{trial_type}.tsv'.format(trial_type=trial_type))
+        copyfile(correlation_matrix, out_file)
+
+        return out_file
+
+    input_node = pe.MapNode(niu.IdentityInterface(fields=['betaseries_files',
+                                                          'atlas_file',
+                                                          'atlas_lut']),
+                            iterfield=['betaseries_files'],
+                            name='input_node')
 
     output_node = pe.Node(niu.IdentityInterface(fields=['correlation_matrix']),
                           name='output_node')
 
-    atlas_corr_node = pe.Node(AtlasConnectivity, name='atlas_corr_node')
+    atlas_corr_node = pe.MapNode(AtlasConnectivity, name='atlas_corr_node', iterfield=['timeseries_file'])
 
+    rename_matrix_node = pe.MapNode(niu.Function(output_names=['correlation_matrix_trialtype'],
+                                                 function=_rename_matrix),
+                                    iterfield=['correlation_matrix', 'betaseries_file'],
+                                    name='rename_matrix_node')
     workflow.connect([
-        (input_node, atlas_corr_node, [('betaseries_file', 'timeseries_file'),
+        (input_node, atlas_corr_node, [('betaseries_files', 'timeseries_file'),
                                        ('atlas_file', 'atlas_file'),
                                        ('atlas_lut', 'atlas_lut')]),
-        (atlas_corr_node, output_node, [('correlation_matrix', 'correlation_matrix')]),
+        (input_node, rename_matrix_node, [('betaseries_files', 'betaseries_file')]),
+        (atlas_corr_node, rename_matrix_node, [('correlation_matrix', 'correlation_matrix')]),
+        (rename_matrix_node, output_node, [('correlation_matrix_trialtype', 'correlation_matrix')]),
     ])
 
     return workflow
