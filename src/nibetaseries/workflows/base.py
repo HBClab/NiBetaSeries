0cc681f9434a5b47586f81049fcd21477b60691c
James Kent <james-kent@uiowa.edu>
2018-07-15 22:35:21 -0500

    refactor base to work with new workflows

--- src/nibetaseries/workflows/base.py
+++ src/nibetaseries/workflows/base.py
@@ -17,9 +17,9 @@ import pkg_resources as pkgr
 from bids.grabbids import BIDSLayout
 
 
-def init_nibetaseries_participant_wf(atlas_img, atlas_lut, bids_dir, confound_column_headers,
+def init_nibetaseries_participant_wf(atlas_img, atlas_lut, bids_dir,
                                      derivatives_pipeline_dir, exclude_variant_label, high_pass, hrf_model, low_pass,
-                                     output_dir, run_label, session_label, slice_time_ref, smoothing_kernel,
+                                     output_dir, run_label, selected_confounds, session_label, smoothing_kernel,
                                      space_label, subject_list, task_label, variant_label, work_dir):
     """
     This workflow organizes the execution of NiBetaSeries, with a sub-workflow for
@@ -107,26 +107,25 @@ def init_nibetaseries_participant_wf(atlas_img, atlas_lut, bids_dir, confound_co
                                                ses=session_label,
                                                space=space_label,
                                                deriv=True)
+
         subject_bids_data = collect_data(bids_layout,
                                          subject_label,
                                          task=task_label,
                                          run=run_label,
                                          ses=session_label)
 
-        # if you want to avoid using the ICA-AROMA variant
+        # if you want to avoid using a particular variant
         if exclude_variant_label:
             subject_derivative_data['preproc'] = [
                 preproc for preproc in subject_derivative_data['preproc'] if exclude_variant_label not in preproc
             ]
+
         # if you only want to use a particular variant
         if variant_label:
             subject_derivative_data['preproc'] = [
                 preproc for preproc in subject_derivative_data['preproc'] if variant_label in preproc
             ]
 
-        # make sure the lists are the same length
-        # pray to god that they are in the same order?
-        # ^they appear to be in the same order
         length = len(subject_derivative_data['preproc'])
 
         if any(len(lst) != length for lst in [subject_derivative_data['brainmask'],
@@ -139,21 +138,14 @@ def init_nibetaseries_participant_wf(atlas_img, atlas_lut, bids_dir, confound_co
             atlas_lut=atlas_lut,
             brainmask_list=subject_derivative_data['brainmask'],
             confound_tsv_list=subject_derivative_data['confounds'],
-            confound_column_headers=confound_column_headers,
             events_tsv_list=subject_bids_data['events'],
             high_pass=high_pass,
             hrf_model=hrf_model,
             low_pass=low_pass,
             name='single_subject' + subject_label + '_wf',
             preproc_img_list=subject_derivative_data['preproc'],
-            run_label=run_label,
-            session_label=ses_label,
+            selected_confounds=selected_confounds,
             smoothing_kernel=smoothing_kernel,
-            space_label=space_label,
-            subject_label=subject_label,
-            slice_time_ref=slice_time_ref,
-            task_label=task_label,
-            variant_label=variant_label,
         )
 
         single_subject_wf.config['execution']['crashdump_dir'] = (
@@ -168,54 +160,50 @@ def init_nibetaseries_participant_wf(atlas_img, atlas_lut, bids_dir, confound_co
     return nibetaseries_participant_wf
 
 
-def init_single_subject_wf(atlas_img, atlas_lut, brainmask_list,
-                           confound_tsv_list, confound_column_headers, events_tsv_list, high_pass,
-                           hrf_model, low_pass, name, preproc_img_list, smoothing_kernel, slice_time_ref):
+def init_single_subject_wf(atlas_img, atlas_lut, brainmask_list, confound_tsv_list, events_tsv_list, high_pass,
+                           hrf_model, low_pass, name, preproc_img_list, selected_confounds, smoothing_kernel):
 
     workflow = pe.Workflow(name=name)
 
     # name the nodes
-    inputnode = pe.Node(niu.IdentityInterface(fields=['brainmask',
-                                                      'confound_tsv',
-                                                      'events_tsv',
-                                                      'preproc_img',
-                                                      'atlas_img',
-                                                      'atlas_lut']),
-                        name='inputnode',
-                        iterables=[('brainmask', brainmask_list),
-                                   ('confound_tsv', confound_tsv_list),
-                                   ('events_tsv', events_tsv_list),
-                                   ('preproc_img', preproc_img_list)],
-                        synchronize=True)
+    input_node = pe.Node(niu.IdentityInterface(fields=['brainmask',
+                                                       'confound_tsv',
+                                                       'events_tsv',
+                                                       'preproc_img',
+                                                       'atlas_img',
+                                                       'atlas_lut']),
+                         name='input_node',
+                         iterables=[('brainmask', brainmask_list),
+                                    ('confound_tsv', confound_tsv_list),
+                                    ('events_tsv', events_tsv_list),
+                                    ('preproc_img', preproc_img_list)],
+                         synchronize=True)
 
-    inputnode.inputs.atlas_img = atlas_img
-    inputnode.inputs.atlas_lut = atlas_lut
+    input_node.inputs.atlas_img = atlas_img
+    input_node.inputs.atlas_lut = atlas_lut
 
-    outputnode = pe.Node(niu.IdentityInterface(fields=['zmaps']),
-                         name='outputnode')
-
+    output_node = pe.Node(niu.IdentityInterface(fields=['correlation_matrix']),
+                          name='output_node')
 
     # initialize the betaseries workflow
     betaseries_wf = init_betaseries_wf(hrf_model=hrf_model, low_pass=low_pass, high_pass=high_pass,
-                                       smoothing_kernel=smoothing_kernel,
-                                       confound_column_headers=confound_column_headers,
-                                       slice_time_ref=slice_time_ref)
+                                       selected_confounds=selected_confounds, smoothing_kernel=smoothing_kernel)
 
     # initialize the analysis workflow
     correlation_wf = init_correlation_wf()
+
     # connect the nodes
     workflow.connect([
-        (inputnode, betaseries_wf,
-            [('preproc_img', 'inputnode.bold_img'),
-             ('events_tsv', 'inputnode.events_tsv'),
-             ('brainmask', 'inputnode.brainmask'),
-             ('confound_tsv', 'inputnode.confound_tsv')]),
+        (input_node, betaseries_wf,
+            [('preproc_img', 'input_node.bold_file'),
+             ('events_tsv', 'input_node.events_file'),
+             ('brainmask', 'input_node.bold_mask_file'),
+             ('confound_tsv', 'input_node.confounds_file')]),
         (betaseries_wf, correlation_wf,
-            [('outputnode.betaseries_img_list', 'inputnode.betaseries_img_list')]),
-        (inputnode, correlation_wf, [('brainmask', 'inputnode.brainmask'),
-                                     ('atlas_img', 'inputnode.atlas_img'),
-                                     ('atlas_lut', 'inputnode.atlas_lut'),
-        (correlation_wf, outputnode, [('outputnode.zmaps', 'zmaps')]),
+            [('output_node.betaseries_files', 'input_node.betaseries_files')]),
+        (input_node, correlation_wf, [('atlas_img', 'input_node.atlas_img'),
+                                     ('atlas_lut', 'input_node.atlas_lut')]),
+        (correlation_wf, output_node, [('output_node.correlation_matrix', 'correlation_matrices')]),
     ])
 
     return workflow
