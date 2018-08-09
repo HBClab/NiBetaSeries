dfb1455fade8ec3f44103e26ed2f62c0d2de8c66
James Kent <james-kent@uiowa.edu>
2018-08-09 10:56:50 -0700

    run versioneer install

--- src/nibetaseries/__init__.py
+++ src/nibetaseries/__init__.py
@@ -2,3 +2,7 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
