27a0101109b8ba94894badbcafd866aaa397857b
James Kent <james-kent@uiowa.edu>
2018-12-08 18:05:52 -0600

    add functional test to test_run.py

--- src/nibetaseries/cli/tests/test_run.py
+++ src/nibetaseries/cli/tests/test_run.py
@@ -1,4 +1,5 @@
-
+from subprocess import call
+import os
 from ..run import get_parser
 
 
@@ -7,3 +8,17 @@ def test_get_parser():
         get_parser().parse_args(['-h'])
     except SystemExit:
         print('success')
+
+
+def test_nibs(bids_dir, atlas_file, atlas_lut, fmriprep_dir):
+    bids_dir = str(bids_dir)
+    out_dir = os.path.join(bids_dir, 'derivatives')
+    assert call(["nibs",
+                 "-a " + str(atlas_file),
+                 "-l " + str(atlas_lut),
+                 "-c WhiteMatter CSF",
+                 "-lp 0.01",
+                 bids_dir,
+                 "fmriprep",
+                 out_dir,
+                 "participant"])
