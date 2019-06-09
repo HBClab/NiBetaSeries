ee03341a8fb6800dacd24074aebcb6c8fbd46782
James Kent <james-kent@uiowa.edu>
2019-06-09 13:27:44 +0200

    only allow pip to install if above 3.5

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
+    python_requires='~=3.5',
     entry_points={
         'console_scripts': [
             'nibs = nibetaseries.cli.run:main',
