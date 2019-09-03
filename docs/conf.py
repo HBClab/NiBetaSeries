aea1b17fa5e41c5c809a38e5055bb98b08f60e39
James Kent <james-kent@uiowa.edu>
2019-09-02 21:48:46 -0500

    add static file path

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,8 @@ html_sidebars = {
 }
 html_short_title = '%s-%s' % (project, version)
 
+html_static_path = ['_static']
+
 napoleon_use_ivar = True
 napoleon_use_rtype = False
 napoleon_use_param = False
