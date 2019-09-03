16ecd68a127df82255f8a5fcc786e4306e463edc
James Kent <james-kent@uiowa.edu>
2019-09-03 07:26:08 +0000

    add cython as dependency (for py3.5?)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import versioneer
 # 30.3.0 allows us to put most metadata in setup.cfg
 # Should match pyproject.toml
 # Not going to help us much without numpy or new pip, but gives us a shot
-SETUP_REQUIRES = ['setuptools >= 40.8']
+SETUP_REQUIRES = ['setuptools >= 40.8', 'cython']
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
