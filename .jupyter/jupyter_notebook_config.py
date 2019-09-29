35accc70de6b324c16bf6a472c8de21959628d0d
James Kent <james-kent@uiowa.edu>
2019-09-29 03:40:24 +0000

    exclude file from flake8

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
 c.ContentsManager.preferred_jupytext_formats_read = "py:sphinx"
 c.ContentsManager.sphinx_convert_rst2md = True
